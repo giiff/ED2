74f45c067483d1f2662fff8de0e47ee3a8bbbefd
Arthur José Fernandes <49981282+arthurjf@users.noreply.github.com>
2022-06-15 12:31:39 -0300

    Algoritmo guloso de mochila finalizado

--- algoritmosGulosos/mochila/mochila.py
+++ algoritmosGulosos/mochila/mochila.py
@@ -1,8 +1,14 @@
+from audioop import reverse
+
+
 class Item:
     def __init__(self, nome, peso, valor):
         self.__nome = nome
         self.__peso = peso
         self.__valor = valor
+
+    def getNome(self):
+        return self.__nome
 
     def getPeso(self):
         return self.__peso
@@ -12,18 +18,27 @@ class Item:
 
 
 class Mochila:
-    def __init__(self, pesoMaximo):
+    def __init__(self, pesoMaximo, itens=[]):
         self.__pesoMaximo = pesoMaximo
+        self.__itens = itens
+
+    def getItens(self):
+        return self.__itens
+
+    def adicionarItem(self, item: Item):
+        self.__itens.append(item)
+
+    def getPesoAtual(self):
+        peso = 0
+        for t in self.__itens:
+            peso += t.getPeso()
+        return peso
 
     def guardarItensPorValor(self, itens=[Item]):
-        itens.append(itens)
-        #itens.sort(key=lambda x: x.getValorItem())
-        solucao = {itens[0]}
-        pesoAtual = solucao[0].getPeso()
+        itens.sort(key=lambda x: x.getValorItem())
         for i in range(1, len(itens), +1):
-            if pesoAtual > self.__pesoMaximo:
-                return solucao
-            if pesoAtual + itens[i].getPeso() > self.__pesoMaximo:
-                continue
-            solucao.add(itens[i])
-        return solucao
+            if self.getPesoAtual() >= self.__pesoMaximo:
+                return
+            if self.getPesoAtual() + itens[i].getPeso() > self.__pesoMaximo:
+                return
+            self.adicionarItem(itens[i])
--- algoritmosGulosos/mochila/prog001.py
+++ algoritmosGulosos/mochila/prog001.py
@@ -6,8 +6,23 @@ i2 = Item("prata", 2, 20)
 i3 = Item("diamante", 8, 800)
 i4 = Item("cobre", 1, 5)
 
-mochila = Mochila(15)
+mochilaPorValor = Mochila(15)
+mochilaPorValor.guardarItensPorValor([i1, i2, i3, i4])
 
-itens = [i1, i2, i3, i4]
-itens.sort(key=lambda x: x.getValorItem())
-mochila.guardarItensPorValor(itens)
+outputTexto = ""
+
+# Mostrando resultado das pedras guardadas
+textoMochila = "Pedras guardadas: "
+for i in mochilaPorValor.getItens():
+    textoMochila += i.getNome() + " "
+print(textoMochila)
+outputTexto += textoMochila + "\n"
+
+# Mostrando o peso atual da mochila
+pesoMochilaTexto = f"Peso mochila: {mochilaPorValor.getPesoAtual()}"
+print(f"{pesoMochilaTexto}")
+outputTexto += pesoMochilaTexto
+
+# Salvando resultados em um arquivo txt
+with open('Output.txt', 'w') as outputArquivo:
+    outputArquivo.write(outputTexto)
