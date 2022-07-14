3d55f040e371e72b03bf5b98f98a8ad588f616d4
JulianoSchaurich <103121235+JulianoSchaurich@users.noreply.github.com>
2022-07-14 17:05:10 -0300

    Add files via upload

--- OtimizacaoCombinatoria/Instituto.py
+++ OtimizacaoCombinatoria/Instituto.py
@@ -1,60 +1,65 @@
 from Professor import Professor
 from Vagao import Vagao
 
-
 class Instituto:
     def __init__(self, vagoes, professores):
         self.__armario = vagoes
         self.__professores = professores
 
+    def getArmario(self):
+        return self.__armario
+
+    def getProfessores(self):
+        return self.__professores
+
     def set_armario(self):
-        professores = self.__professores
-        professores.sort(key=lambda x: x.getAltura(), reverse=True)
+        p_disponivel = self.getProfessores()
+        p_disponivel.sort(key=lambda x: x.getAltura(), reverse=True)
 
-        for i in range(len(self.__armario)):
-            for j in range(len(self.__armario[i])):
-                professores = self.inserir_professor(professores, i, j)
+        for i in range(len(self.getArmario())):
+            for j in range(len(self.getArmario()[i])):
+                p_disponivel = self.inserir_professor(p_disponivel, i, j)
 
-    def inserir_professor(self, professores, i, j):
-        for k in range(len(professores)):
-            if self.__armario[i][j].getCodigo() in professores[k].getPreferencia():
-                self.__armario[i][j].ocupar(professores[k].getNome())
-                del professores[k]
-                return professores
-        if (self.__armario[i][j].getOcupado() == False):
-            self.__armario[i][j].ocupar(professores[0].getNome())
-            del professores[0]
-            return professores
+    def inserir_professor(self, p_disponivel, i, j):
+        for k in range(len(p_disponivel)):
+            if self.getArmario()[i][j].getCodigo() in p_disponivel[k].getPreferencia():
+                self.__armario[i][j].ocupar(p_disponivel[k].getNome())
+                del p_disponivel[k]
+                return p_disponivel
+        if (self.getArmario()[i][j].getOcupado() == False):
+            self.__armario[i][j].ocupar(p_disponivel[0].getNome())
+            del p_disponivel[0]
+            return p_disponivel
 
-    def set_armario2(self):
-        professores2 = self.__professores
-        professores2.sort(key=lambda x: x.getAltura(), reverse=True)
+    def set_armario_preferencia(self):
+        p_disponivel = self.getProfessores()
+        p_disponivel.sort(key=lambda x: x.getAltura(), reverse=True)
 
-        for i in range(len(self.__armario)):
-            for j in range(len(self.__armario[i])):
-                professores2 = self.inserir_professor_preferencia(professores2, i, j)
+        for i in range(len(self.getArmario())):
+            for j in range(len(self.getArmario()[i])):
+                p_disponivel = self.inserir_professor_preferencia(p_disponivel, i, j)
 
-        for i in range(len(self.__armario)):
-            for j in range(len(self.__armario[i])):
-                if self.__armario[i][j].getOcupado() == False:
-                    self.__armario[i][j].ocupar(professores2[0].getNome())
-                    del professores2[0]
+        for i in range(len(self.getArmario())):
+            for j in range(len(self.getArmario()[i])):
+                if self.getArmario()[i][j].getOcupado() == False:
+                    self.getArmario()[i][j].ocupar(p_disponivel[0].getNome())
+                    del p_disponivel[0]
 
-    def inserir_professor_preferencia(self, professores, i, j):
-        for k in range(len(professores)):
-            if self.__armario[i][j].getCodigo() in professores[k].getPreferencia():
-                self.__armario[i][j].ocupar(professores[k].getNome())
-                del professores[k]
-                return professores
-        return professores
+    def inserir_professor_preferencia(self, p_disponivel, i, j):
+        for k in range(len(p_disponivel)):
+            if self.getArmario()[i][j].getCodigo() in p_disponivel[k].getPreferencia():
+                self.getArmario()[i][j].ocupar(p_disponivel[k].getNome())
+                del p_disponivel[k]
+                return p_disponivel
+        return p_disponivel
 
     def desocupar_armario(self):
-        for i in range(len(self.__armario)):
-            for j in range(len(self.__armario[i])):
-                self.__armario[i][j].desocupar()
+        for i in range(len(self.getArmario())):
+            for j in range(len(self.getArmario()[i])):
+                self.getArmario()[i][j].desocupar()
 
     def imprimir_armario(self):
         print("ARMARIO:")
-        for i in range(len(self.__armario)):
-            for j in range(len(self.__armario[i])):
-                print(self.__armario[i][j].getCodigo(), " - ", self.__armario[i][j].getProfessor())
+        for i in range(len(self.getArmario())):
+            for j in range(len(self.getArmario()[i])):
+                print(self.getArmario()[i][j].getCodigo(), " - ", self.getArmario()[i][j].getProfessor())
--- OtimizacaoCombinatoria/main.py
+++ OtimizacaoCombinatoria/main.py
@@ -3,12 +3,24 @@ from Instituto import Instituto
 from Professor import Professor
 from Vagao import Vagao
 
-vagoes = [
+v = [
   [Vagao(123), Vagao(745)],
   [Vagao(894), Vagao(713)],
   [Vagao(333), Vagao(222)]
 ]
-professores = [
+v2 = [
+  [Vagao(123), Vagao(745)],
+  [Vagao(894), Vagao(713)],
+  [Vagao(333), Vagao(222)]
+]
+p = [
+  Professor("Afrânio", 185, [894]),
+  Professor("Jaime", 181, [222, 745]),
+  Professor("Samara", 174, [894, 894]),
+  Professor("Ugo", 175, [745, 894]),
+  Professor("Maria", 174, [713]),
+  Professor("Dama de Paus", 195, [222])]
+p2 = [
   Professor("Afrânio", 185, [894]),
   Professor("Jaime", 181, [222, 745]),
   Professor("Samara", 174, [894, 894]),
@@ -17,12 +29,11 @@ professores = [
   Professor("Dama de Paus", 195, [222])]
 
 
+i = Instituto(v, p)
+i.set_armario()
+i.imprimir_armario()
 
-#i = Instituto(vagoes, professores)
-#i.set_armario()
-#i.imprimir_armario()
-
-i2 = Instituto(vagoes, professores)
-i2.set_armario2()
+i2 = Instituto(v2, p2)
+i2.set_armario_preferencia()
 i2.imprimir_armario()
 
